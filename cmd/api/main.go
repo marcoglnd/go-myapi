f5dc29d4e18f0dbef2a7f52e826ec9d9632a6756
Marco Galindo <marcoantonio.galindo@mercadolivre.com>
2022-07-20 10:49:50 -0300

    refactor: Change folder architecture

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/marcoglnd/go-myapi/controller"
+	controller "github.com/marcoglnd/go-myapi/internal/webapp"
 )
 
 type Response struct {
